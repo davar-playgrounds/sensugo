51bfb82fed489207e950da0c9c034d4dd262e084
Runar Fredagsvik <runarsf@protonmail.com>
2020-02-11 08:29:30 +0000

    Working

--- default-data/scripts/sensu-backend.sh
+++ default-data/scripts/sensu-backend.sh
@@ -3,7 +3,7 @@
 set -o monitor
 
 # Start the backend in the background
-./bin/sensu-backend start --state-dir /var/lib/sensu/sensu-backend --log-level debug &
+./bin/sensu-backend start --state-dir /var/lib/sensu/sensu-backend --log-level info & # log level debug
 # Initialize the backend (has to be done while the backend is running)
 ./bin/sensu-backend init
 
